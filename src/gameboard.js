c044bf41c0eaa82833f9612d686c3c6800f8307a
ehedei <ehedeihg@gmail.com>
2021-05-27 07:29:07 +0100

    Code refactor

--- src/gameboard.js
+++ src/gameboard.js
@@ -10,6 +10,7 @@ export function GameBoard (playerName, player) {
   this.level = 1
   this.timerId = null
   this.board = new Board(playerName, this)
+  this.wrapper = document.getElementById(playerName + '-zone')
 }
 
 GameBoard.prototype.run = function (newSpeed = this.speed) {
@@ -95,7 +96,7 @@ GameBoard.prototype.increaseLevel = function () {
     this.timerId = false
     this.speed = this.speed - (this.level - 1) * GAME_PARAMS.acceleration
     this.run()
-    const label = document.getElementById('level-up')
+    const label = this.wrapper.querySelector('.player-alert')
     label.classList.add('level-up')
 
     setTimeout(function () {
@@ -132,6 +133,108 @@ GameBoard.prototype.saveBlocks = function (special = null) {
   }, GAME_PARAMS.initialSpeed * 1.5)
 }
 
+GameBoard.prototype.prepareDeletions = function () {
+  // Vertical
+  const verticalDelete = this.calculateVerticalDeletions()
+
+  // Horizontal
+  const horizontalDelete = this.calculateHorizontalDeletions()
+
+  // Diagonal
+  const diagonalDelete = this.calculateDiagonalDeletions()
+
+  // Diagonal Inversa
+  const inverseDiagonalDelete = this.calculateInverseDiagonalDeletions()
+
+  return (verticalDelete || horizontalDelete || diagonalDelete || inverseDiagonalDelete)
+}
+
+GameBoard.prototype.calculateVerticalDeletions = function () {
+  const columns = this.board.columns
+  let needToDelete = false
+
+  // Vertical
+  for (let x = 0; x < columns.length; x++) {
+    for (let y = 0; y < columns[x].length - 2; y++) {
+      if (!columns[x][y] || !columns[x][y + 1] || !columns[x][y + 2]) {
+        break
+      } else if (columns[x][y].type === columns[x][y + 1].type && columns[x][y].type === columns[x][y + 2].type) {
+        columns[x][y].erasable = true
+        columns[x][y + 1].erasable = true
+        columns[x][y + 2].erasable = true
+        needToDelete = true
+      }
+    }
+  }
+
+  return needToDelete
+}
+
+GameBoard.prototype.calculateHorizontalDeletions = function () {
+  const columns = this.board.columns
+  let needToDelete = false
+
+  // Horizontal
+  for (let x = 0; x < columns.length - 2; x++) {
+    for (let y = 0; y < columns[x].length; y++) {
+      if (!columns[x][y] || !columns[x + 1][y] || !columns[x + 2][y]) {
+        break
+      } else if (columns[x][y].type === columns[x + 1][y].type && columns[x + 2][y].type === columns[x][y].type) {
+        columns[x][y].erasable = true
+        columns[x + 1][y].erasable = true
+        columns[x + 2][y].erasable = true
+        needToDelete = true
+      }
+    }
+  }
+
+  return needToDelete
+}
+
+GameBoard.prototype.calculateDiagonalDeletions = function () {
+  const columns = this.board.columns
+  let needToDelete = false
+
+  // Diagonal
+  for (let x = 0; x < columns.length - 2; x++) {
+    for (let y = 0; y < columns[x].length; y++) {
+      if (!columns[x][y] || !columns[x + 1][y + 1] || !columns[x + 2][y + 2]) {
+        break
+      } else if (columns[x][y].type === columns[x + 1][y + 1].type && columns[x + 2][y + 2].type === columns[x][y].type) {
+        columns[x][y].erasable = true
+        columns[x + 1][y + 1].erasable = true
+        columns[x + 2][y + 2].erasable = true
+        needToDelete = true
+      }
+    }
+  }
+
+  return needToDelete
+}
+
+GameBoard.prototype.calculateInverseDiagonalDeletions = function () {
+  const columns = this.board.columns
+  let needToDelete = false
+
+  // Inverse Diagonal
+  for (let x = 2; x < columns.length; x++) {
+    for (let y = 0; y < columns[x].length; y++) {
+      if (!columns[x][y] || !columns[x - 1][y + 1] || !columns[x - 2][y + 2]) {
+        break
+      } else if (columns[x][y].type === columns[x - 1][y + 1].type && columns[x - 2][y + 2].type === columns[x][y].type) {
+        columns[x][y].erasable = true
+        columns[x - 1][y + 1].erasable = true
+        columns[x - 2][y + 2].erasable = true
+        needToDelete = true
+      }
+    }
+  }
+
+  return needToDelete
+}
+
+
+/*
 GameBoard.prototype.prepareDeletions = function () {
   const columns = this.board.columns
   let needToDelete = false
@@ -193,44 +296,4 @@ GameBoard.prototype.prepareDeletions = function () {
   }
   return needToDelete
 }
-
-/*
-GameBoard.prototype.calculateDeletions = function (baseX, baseY) {
-  const columns = this.board.columns
-  let needToDelete = false
-
-  console.log(baseX)
-  console.log(baseY)
-
-  for (let x = 0; x < columns.length; x++) {
-    for (let y = 0; y < columns[x].length; y++) {
-      if (!columns[x][y] || !columns[x + baseX.a][y + baseY.a] || !columns[x + baseX.a][y + baseY.b]) {
-        break
-      } else if (columns[x][y].type === columns[x][y + a].type && columns[x][y + b].type === columns[x][y].type) {
-        columns[x][y].erasable = true
-        columns[x][y + a].erasable = true
-        columns[x][y + b].erasable = true
-        needToDelete = true
-      }
-    }
-  }
-  return needToDelete
-}
-
-GameBoard.prototype.prepareDeletions = function () {
-  // Vertical
-  const verticalDelete = this.calculateDeletions({ a: 1, b: 2 }, { a: 0, b: 0 })
-
-  // Horizontal
-  const horizontalDelete = this.calculateDeletions({ a: 1, b: -1 }, { a: 0, b: 0 })
-
-  // Diagonal
-  const diagonalDelete = this.calculateDeletions({ a: 1, b: 2 }, { a: 1, b: 2 })
-
-  // Diagonal Inversa
-  const inverseDiagonalDelete = this.calculateDeletions({ a: -1, b: -2 }, { a: 1, b: 2 })
-
-  return (verticalDelete || horizontalDelete || diagonalDelete || inverseDiagonalDelete)
-}
-
 */
